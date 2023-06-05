a619f9d44f2688eb4caa8ecbdbf0e76dfc50623d
tbrown034 <tbrown034@gmail.com>
2023-06-05 01:44:14 -0500

    feat(board): added board and chess logic from react-chessboard and chess.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
+"use client";
+import React, { useState } from "react";
+import Board from "./puzzle/Board.jsx";
+
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-zinc-400">
-      Hello World
+    <main className="flex flex-col items-center justify-center min-h-screen gap-14 bg-zinc-400">
+      <div className="text-3xl text-blue-500">My Chess App</div>
+      <Board />
     </main>
   );
 }
